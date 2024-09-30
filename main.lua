63c7b23d860232001d99584e7b9a680059313e76
nexer1234 <77268699+Pro666Pro@users.noreply.github.com>
2024-09-30 16:08:34 +0900

    Update main.lua

--- main.lua
+++ main.lua
@@ -23,12 +23,56 @@ firetouchinterest(game.Players.LocalPlayer.Character:WaitForChild("Head"), v, 1)
                     end
                 end
                 end
+
+if game.Workspace:FindFirstChild("HonorOrb") then
 for i,v in pairs(game.Workspace:GetChildren()) do
-                    if v.Name == "JetOrb" or v.Name == "PhaseOrb" or v.Name == "GlitchOrb" or v.Name == "HonorOrb" then
+                    if v.Name == "HonorOrb" then
 firetouchinterest(game.Players.LocalPlayer.Character:WaitForChild("Head"), v, 0)
 firetouchinterest(game.Players.LocalPlayer.Character:WaitForChild("Head"), v, 1)
                     end
                 end
+                end
+
+for i,v in pairs(game.Workspace:GetChildren()) do
+                    if v.Name == "JetOrb" or v.Name == "PhaseOrb" or v.Name == "GlitchOrb" then
+firetouchinterest(game.Players.LocalPlayer.Character:WaitForChild("Head"), v, 0)
+firetouchinterest(game.Players.LocalPlayer.Character:WaitForChild("Head"), v, 1)
+                    end
+                end
+
+if game.Workspace:FindFirstChild("Toolbox") then
+for i,v in pairs(game.Workspace:GetDescendants()) do
+                    if v.Name == "Toolbox" and v:FindFirstChild("ClickDetector") then
+fireclickdetector(v.ClickDetector, 0)
+fireclickdetector(v.ClickDetector, 1)
+                    end
+                end
+            end
+
+if game:GetService("BadgeService"):UserHasBadgeAsync(game.Players.LocalPlayer.UserId, 2124930374) then
+game:GetService("StarterGui"):SetCore("SendNotification",{Title = "jet glove",Text = "you got jet gloev" ,Duration = 10, Icon = "rbxthumb://type=Asset&id=9649923610&w=150&h=150",Button1 = "k"})
+end
+
+if game:GetService("BadgeService"):UserHasBadgeAsync(game.Players.LocalPlayer.UserId, 2124886261) then
+game:GetService("StarterGui"):SetCore("SendNotification",{Title = "phase glove",Text = "you got phase gloev" ,Duration = 10, Icon = "rbxthumb://type=Asset&id=9649923610&w=150&h=150",Button1 = "k"})
+end
+
+if game:GetService("BadgeService"):UserHasBadgeAsync(game.Players.LocalPlayer.UserId, 2128012376) then
+game:GetService("StarterGui"):SetCore("SendNotification",{Title = "slapple glove",Text = "you got slapple gloev" ,Duration = 10, Icon = "rbxthumb://type=Asset&id=9649923610&w=150&h=150",Button1 = "k"})
+end
+
+if game:GetService("BadgeService"):UserHasBadgeAsync(game.Players.LocalPlayer.UserId, 2146540403) then
+game:GetService("StarterGui"):SetCore("SendNotification",{Title = "hammer glove",Text = "you got hammer gloev" ,Duration = 10, Icon = "rbxthumb://type=Asset&id=9649923610&w=150&h=150",Button1 = "k"})
+end
+
+if game:GetService("BadgeService"):UserHasBadgeAsync(game.Players.LocalPlayer.UserId, 187186758930457) then
+game:GetService("StarterGui"):SetCore("SendNotification",{Title = "siphon glove",Text = "you got siphon gloev" ,Duration = 10, Icon = "rbxthumb://type=Asset&id=9649923610&w=150&h=150",Button1 = "k"})
+end
+
+if game:GetService("BadgeService"):UserHasBadgeAsync(game.Players.LocalPlayer.UserId, 4256352459948010) then
+game:GetService("StarterGui"):SetCore("SendNotification",{Title = "materialize glove",Text = "you got materialize gloev" ,Duration = 10, Icon = "rbxthumb://type=Asset&id=9649923610&w=150&h=150",Button1 = "k"})
+end
+
 local serverList = {}
 for _, v in ipairs(game:GetService("HttpService"):JSONDecode(game:HttpGetAsync("https://games.roblox.com/v1/games/" .. game.PlaceId .. "/servers/Public?sortOrder=Asc&limit=100")).data) do
 	if v.playing and type(v) == "table" and v.maxPlayers > v.playing and v.id ~= game.JobId then
